3b2ca56ed4562415f4f25eb26bb80f9e4b98cb92
Ethan Houston <ethan.houston@gmail.com>
2018-03-17 17:01:16 -0700

    email list

---
+++ js/home.js
@@ -0,0 +1,45 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyDWgGOZMOlalyCGIgHLzYw5KwqcFoBUqY4",
+    authDomain: "kassiakarras-1fcbf.firebaseapp.com",
+    databaseURL: "https://kassiakarras-1fcbf.firebaseio.com",
+    projectId: "kassiakarras-1fcbf",
+    storageBucket: "kassiakarras-1fcbf.appspot.com",
+    messagingSenderId: "942270893396"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+let writeEmail = () => {
+
+    let email = document.getElementById(`message`).value;
+
+    database.ref(`mailing_list`).push({
+
+        email: email
+
+    });
+};
+
+let getEmails = () => {
+
+    const ref = database.ref(`mailing_list`);
+
+    let emails = [];
+
+    ref.on(`value`, function (snapshot) {
+        snapshot.forEach(function (child) {
+
+            emails.push(child.val()[`email`]);
+
+
+        })
+    });
+
+    console.log(emails);
+
+};
+
+getEmails()
